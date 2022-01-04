972ed4711a92438360a06ea602065efc25336636
GeysonMes <geysonsousa17@gmail.com>
2022-01-04 15:57:50 -0300

    exercicio 12 revisao

--- modulo1/reforco-js-lista2/exercicios.js
+++ modulo1/reforco-js-lista2/exercicios.js
@@ -166,7 +166,11 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+    return {
+        ...pessoa,
+        nome: "ANÔNIMO"
+      };
+
 }
 
 // EXERCÍCIO 13A
