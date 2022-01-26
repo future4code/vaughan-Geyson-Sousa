4e362a1b4626ee1e5a900fe1d1f4ef4a884c70f2
GeysonMes <geysonsousa17@gmail.com>
2022-01-26 17:25:55 -0300

    labenusers- completo

--- modulo2/apis-labenusers/labenusers/src/App.js
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -1,83 +1,39 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
-
+import TelaCadastro from "./components/TelaCadastro";
+import TelaListaUsuario from "./components/TelaListaUsuario";
 
 
 
 class App extends React.Component {
-  state = {
-    users: [],
-    inputNome: "",
-    inputEmail: "",
+  state ={
+    telaInicial: "cadastro"
   }
-  componentDidMount = () => {
-    this.getAllUsers();
-  };
-  onChangeNome = (e) => {
-    this.setState({ inputNome: e.target.value });
-  };
-  onChangeEmail = (e) => {
-    this.setState({ inputEmail: e.target.value });
-  };
-  getAllUsers = () => {
-    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-    const axioConfig = { headers: { Authorization: "geyson-sousa-vaughan" } }
-    axios
-      .get(url, axioConfig)
-      .then((response) => {
-        this.setState({ users: response.data.result.list });
-        console.log(response.data.result.list);
-      })
-      .catch((error) => {
-        console.log(error.response);
-      })
-  };
-  createUser = () => {
-    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
-    const body = {
-      name: this.state.inputNome,
-      email: this.state.inputEmail,
-    };
-    const axioConfig = { headers: { Authorization: "geyson-sousa-vaughan" } }
-    axios
-      .post(url, body, axioConfig)
-      .then(() => {
-        alert("Usuário criado com sucesso!");
-        this.setState({ inputNome: "" });
-        this.setState({ inputEmail: "" });
-        this.getAllUsers();
-      })
-      .catch((error) => {
-        console.log(error.response);
-        alert("Erro ao criar o usuário!")
-      })
-  };
-  render() {
-    const listUsers = this.state.users.map((lista) => {
-      return (
-        <div>
-          <li>{lista.nome}</li>
-        </div>
-      )
-    });
-    console.log(this.state)
-    return (
-      <div>
-        <input
-          placeholder="Nome"
-          value={this.state.inputNome}
-          onChange={this.onChangeNome}
-        />
-        <input
-          placeholder="E-mail"
-          value={this.state.inputEmail}
-          onChange={this.onChangeEmail}
-        />
-        <button onClick={this.createUser}>Criar Usuário</button>
-        {listUsers}
-      </div>
-    )
+
+  mudarTela =() =>{
+    switch(this.state.telaInicial){
+      case "cadastro":
+        return <TelaCadastro irParaListaUsuario={this.irParaListaUsuario}/>
+        case "lista":
+         return <TelaListaUsuario irParaCadastro ={this.irParaCadastro}/>
+         default:
+           return <div> Erro! Página não encontrada :( </div>
+    }
   }
+  irParaCadastro =()=>{
+    this.setState({telaInicial:"cadastro"})
+  }
+  irParaListaUsuario =()=>{
+    this.setState({telaInicial:"lista"})
+  }
+
+  render(){
+  return (
+     <div>
+       {this.mudarTela()}
+     </div>
+  )
+}
 }
 export default App;
