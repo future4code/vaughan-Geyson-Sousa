eb98e42ab2d8b3e0cfe48a8d477f2a0f616c820a
GeysonMes <geysonsousa17@gmail.com>
2022-03-24 17:32:54 -0300

    aprofundamento-typescript

---
+++ modulo4/aprofundamento-typescript/src/Exercicio1.ts
@@ -0,0 +1,36 @@
+//A)
+//let minhaString : string = 23  da erro pois esperava uma string
+
+//B)
+//let meuNumero:number | string =23 
+
+//C)
+// type Person = {
+//     name:string,
+//     idade:number,
+//     corFavorita: "vermelho" | "laranja" | "amarelo" | "verde" | "azul" | "anil" | "violeta";
+// }
+// let person1:Person={
+//     name:"Geyson",
+//     idade:21,
+//     corFavorita:"vermelho"
+// }
+// let person2:Person={
+//     name:"Karine",
+//     idade:20,
+//     corFavorita:"vermelho"
+// }
+// let person3:Person={
+//     name:"Gledson",
+//     idade:20,
+//     corFavorita:"preto"
+// }
+
+// D)
+// let newPerson:Person={
+//     name:"Karol",
+//     idade:24,
+//     corFavorita:"verde"
+// }
+ 
+
