7f86a2c5ccc1e77afcd7b60d789bf9984f2bd573
GeysonMes <geysonsousa17@gmail.com>
2022-06-08 17:48:39 -0300

    inicio

--- modulo6/rodada-cases-semana4/brainn/src/Pages/HomePage/HomePage.js
+++ modulo6/rodada-cases-semana4/brainn/src/Pages/HomePage/HomePage.js
@@ -1,25 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import parte from '../../Assets/parte.png'
 import LogoSena from '../../Assets/LogoSena.png'
 import megasena from '../../Assets/megasena.png'
 import {Container,Block1,Block2} from "./Styled"
 import {BASE_URL} from '../../Constants/Urls'
 import UseRequestData from '../../Hooks/UseRequestData'
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+
 
 const HomePage = () => {
-    const data= UseRequestData([], `${BASE_URL}/loterias`)
-    console.log(data[0])
+    const data = UseRequestData([], `${BASE_URL}/loterias`)
+    const dataSortion = UseRequestData([], `${BASE_URL}/loterias-concursos`)
+    const [loteriesData, setLoteriasData] = useState("")
+    
 
+    const handleSort =(ev)=>{
+      setLoteriasData(ev.target.value)
+    }
+    const mapData = data[0] && data[0].map((lot)=>{
+      return(
+          
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Sorteio</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={loteriesData}
+            onChange={handleSort}
+            label="Sorteio"
+            defaultValue={data?.[0]?.nome}
+          >
+            <MenuItem value={10}>{lot.nome}</MenuItem>
+            <MenuItem value={20}>{lot.nome}</MenuItem>
+            <MenuItem value={30}>{lot.nome}</MenuItem>
+          </Select>
+        </FormControl>
+          
+    );
+      
+    })
   return (
+    
     <Container>
+       {mapData}
+
       <Block1>
+      
          <img src ={parte}/>
-          <Block2>
          <img src ={LogoSena}/>
          <img src ={megasena}/> 
-         </Block2>
+       
          </Block1>
-   
+
+         
     </Container>
   )
 }
